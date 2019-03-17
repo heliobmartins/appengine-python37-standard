6f4d0dcaf71d637a03536be8d127013222b9728a
Helio Baptista Martins <hbm.helio@gmail.com>
2019-03-18 09:18:51 +1100

    Created project structure

---
+++ api/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+import firebase_admin
+from firebase_admin import firestore, credentials
+from flask import Flask
+
+from api.api_definition import register_apis, initialise_apis
+from api.author_api import author_api
+from settings import config
+
+
+def create_app():
+    app = Flask(__name__)
+    app.db = initialise_firestore()
+    initialise_apis(app)
+    return app
+
+
+def initialise_firestore():
+    if os.getenv('GAE_ENV', '').startswith('standard'):
+        # Production in the standard environment
+        cred = credentials.ApplicationDefault()
+        firebase_admin.initialize_app(cred, {'projectId': config.PROJECT_ID})
+        print("Running on AppEngine")
+    else:
+        # Production in the standard environment
+        cred = credentials.Certificate(config.GOOGLE_APPLICATION_CREDENTIALS)
+        firebase_admin.initialize_app(cred)
+        print("Running on Devserver")
+    return firestore.client()

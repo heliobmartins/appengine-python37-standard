17fc0221034d93dc5c6672e3422afc6833da5b27
Helio Baptista Martins <hbm.helio@gmail.com>
2019-03-21 15:20:02 +1100

    Using decorator api instead of blueprints declarations

--- api/author_api.py
+++ api/author_api.py
@@ -1,35 +1,34 @@
 import flask
-from flask import Blueprint
 
 from messages.author_messages import NewAuthorRequest, UpdateAuthorRequest
 from services.author_service import AuthorService
-
-author_api = Blueprint('author_api', __name__)
-
-
-# TODO 7 - Lot of service = AuthorService.get_instance(). Should I create a class and use it only once? (^.-)
-
-@author_api.route('/authors', methods=['POST'])
-def new_author():
-    service = AuthorService.get_instance()
-    return service.create_author(NewAuthorRequest(flask.request.json))
+from settings.decorators.api_decorator import api, route
 
 
-@author_api.route('/authors/<id>')
-def read_user(id):
-    service = AuthorService.get_instance()  # TODO 9 - How to assure that ID has been filled?
-    return service.get_author(id)
+# noinspection PyMethodParameters
+@api(name='author_api', url_prefix='authors', version="v1")
+class AuthorApi:
 
+    # TODO 7 - Lot of service = AuthorService.get_instance(). Should I create a class and use it only once? (^.-)
 
-@author_api.route('/authors/<id>', methods=['PUT'])
-def update_user(id):
-    service = AuthorService.get_instance()
-    request = UpdateAuthorRequest(flask.request.json)
-    request.id = id
-    return service.update_author(UpdateAuthorRequest(request))
+    @route(methods=['POST'])
+    def new_author():
+        service = AuthorService.get_instance()
+        return service.create_author(NewAuthorRequest(flask.request.json))
 
+    @route(path='<id>', methods=['GET'])
+    def read_author(id):
+        service = AuthorService.get_instance()  # TODO 9 - How to assure that ID has been filled?
+        return service.get_author(id)
 
-@author_api.route('/authors/<id>', methods=['DELETE'])
-def delete_user(id):
-    service = AuthorService.get_instance()
-    return service.delete_author(id)
+    @route(path='/<id>', methods=['PUT'])
+    def update_author(id):
+        service = AuthorService.get_instance()
+        request = UpdateAuthorRequest(flask.request.json)
+        request.id = id
+        return service.update_author(UpdateAuthorRequest(request))
+
+    @route(path='/<id>', methods=['DELETE'])
+    def delete_author(id):
+        service = AuthorService.get_instance()
+        return service.delete_author(id)
--- api/content_api.py
+++ api/content_api.py
@@ -1,13 +1,14 @@
 import flask
-from flask import Blueprint
 
 from messages.content_messages import NewContentRequest
 from services.content_service import ContentService
+from settings.decorators.api_decorator import api, route
 
-content_api = Blueprint('content_api', __name__)
 
-
-@content_api.route('/contents', methods=['POST'])
-def new_author():
-    service = ContentService.get_instance()
-    return service.create_content(NewContentRequest(flask.request.json))
+# noinspection PyMethodParameters
+@api(name="content_api", url_prefix="contents", version="v1")
+class ContentApi:
+    @route(methods=['POST'])
+    def new_content():
+        service = ContentService.get_instance()
+        return service.create_content(NewContentRequest(flask.request.json))
